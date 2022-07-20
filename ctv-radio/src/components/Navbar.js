db7c91bcbe0776508789f37984a8393b6dc1359e
Dee <dgrowette@gmail.com>
2022-07-20 13:13:08 -0700

    added sections to navbar and added botton.js and .css files

--- ctv-radio/src/components/Navbar.js
+++ ctv-radio/src/components/Navbar.js
@@ -1,16 +1,56 @@
 import React, { useState } from "react";
-import { Link } from 'react-router-dom';
-
-
+import { Link } from "react-router-dom";
 
 function Navbar() {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
   return (
     <>
       <nav className="navbar">
         <div className="navbar-container">
-            <Link to="/" className="navbar-logo">
-                CTV Radio <image href=""></image>
-            </Link>
+          <Link to="/" className="navbar-logo">
+            CTV Radio {/* image placeholder <image href=""></image> */}
+          </Link>
+          <div className="menu-icon" onCLick={handleClick}>
+            <i className={click ? "fas fa-times" : "fas fa-bars"} />
+          </div>
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <Link to="/" className="nave-links" onClick={closeMobileMenu}>
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/store"
+                className="nave-links"
+                onClick={closeMobileMenu}
+              >
+                Store
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/support"
+                className="nave-links"
+                onClick={closeMobileMenu}
+              >
+                Support
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/contact"
+                className="nave-links"
+                onClick={closeMobileMenu}
+              >
+                Contact
+              </Link>
+            </li>
+          </ul>
         </div>
       </nav>
     </>
