c4af9b6554034ee55ea0f916deaf64442054f13b
Dee <dgrowette@gmail.com>
2022-07-20 13:24:48 -0700

    save before a restart of vs code

--- ctv-radio/src/components/Button.js
+++ ctv-radio/src/components/Button.js
@@ -1,1 +1,15 @@
-import React from 'react';
+import React from "react";
+import "../components/Button.css";
+import { Link } from "react-router-dom";
+
+const STYLES = ["btn--primary", "btn--outline"];
+
+const SIZES = ["btn--medium", "btn--large"];
+
+export const Button = ({ clidren, type, onClick, buttonStyle, buttonSize }) => {
+  const checkButtonStyle = STYLES.includes(buttonStyle)
+    ? buttonStyle
+    : STYLES[0];
+
+  check;
+};
